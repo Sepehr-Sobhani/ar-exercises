279045e42610cf22fc166e42baf9864e1d9a0914
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-02-02 00:04:28 -0800

    Add validations to each model

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,18 @@
 
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+  validate :men_or_women
+  
+
+  def men_or_women
+    if !mens_apparel && !womens_apparel
+      errors.add(:base, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end 
+
 end
