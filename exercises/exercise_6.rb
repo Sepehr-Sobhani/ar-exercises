171d3a75c97d301502d85ddf57f471acf6faae5b
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-02-01 22:26:42 -0800

    Link store and employee tables task 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,15 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 50)
+@store1.employees.create(first_name: "Sepehr", last_name: "Sobhani", hourly_rate: 55)
+
+
+
+@store2.employees.create(first_name: "Mike", last_name: "Denver", hourly_rate: 60)
+@store2.employees.create(first_name: "Bob", last_name: "Jones", hourly_rate: 55)
+@store2.employees.create(first_name: "Jack", last_name: "Forest", hourly_rate: 50)
+@store2.employees.create(first_name: "Cat", last_name: "Dawson", hourly_rate: 45)
+
