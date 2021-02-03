c358c7080aaebf6df1bdae5b93b20ca612d6b00b
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-02-03 00:51:58 -0800

    Add callback before destroying stores with more than 1 staff

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,9 +3,21 @@ class Employee < ActiveRecord::Base
   validates_associated :store
   validates :first_name, :last_name, presence: true
   validates :hourly_rate, numericality: {
-     greater_than_or_equal_to: 40,
+    greater_than_or_equal_to: 40,
     less_than_or_equal_to: 200,
-     only_integer: true 
-    }
-
+    only_integer: true
+  }
+  # for comparing after and before creating
+  # after_create :create_password
+  
+  before_create :create_password
+  
+  private
+  
+  def create_password
+    self.password = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
+    
+    # for comparing after and before creating
+    # self.save
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -8,11 +8,20 @@ class Store < ActiveRecord::Base
   }
   validate :men_or_women
   
+  before_destroy :without_staff?, prepend: true
+
+
+
+  private 
 
   def men_or_women
     if !mens_apparel && !womens_apparel
       errors.add(:base, "Stores must carry at least one of the men's or women's apparel")
     end
   end 
+
+  def without_staff?
+    Store.find(self.id).employees.size < 1
+  end
 
 end
